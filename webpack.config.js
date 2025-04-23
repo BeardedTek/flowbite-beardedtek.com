999013d72e6e3d88cd2b5ca1ab3a74dea9b44151
BeardedTek <kenny@beardedtek.com>
2025-04-22 21:39:18 -0800

    Update Contact Form to use a dropdown for subject and proper links for Terms of Service and Privacy Policy

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
       new TerserPlugin({
         terserOptions:{
           compress: {
-            drop_console: true,
+            drop_console: false,
           }
         }
       }),
